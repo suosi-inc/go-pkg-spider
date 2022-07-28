cc61891d1cf578f3fabc663839a854b51c691cf7
smallmenu <smallmenu@gmail.com>
2022-07-28 15:00:10 +0800

    * update

--- icp_test.go
+++ icp_test.go
@@ -1,10 +1,38 @@
 package spider
 
 import (
+	"bytes"
 	"testing"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func TestIcp(t *testing.T) {
+func TestDetectIcp(t *testing.T) {
+	var urlStrs = []string{
+		"http://suosi.com.cn",
+		"https://www.163.com",
+		"https://www.sohu.com",
+		"https://www.qq.com",
+		"https://www.hexun.com",
+		"https://www.wfmc.edu.cn/",
+		"https://www.cankaoxiaoxi.com/",
+	}
+
+	for _, urlStr := range urlStrs {
+
+		resp, err := HttpGetResp(urlStr, nil, 30000)
+
+		t.Log(err)
+		t.Log(urlStr)
+
+		doc, _ := goquery.NewDocumentFromReader(bytes.NewReader(resp.Body))
+		doc.Find("noscript,style,iframe,br,link,svg").Remove()
+		icp, loc := Icp(doc)
+		t.Log(icp, loc)
+	}
+}
+
+func TestIcpFromText(t *testing.T) {
 	texts := []string{
 		"粤ICP备17055554号",
 		"粤ICP备17055554-34号",
