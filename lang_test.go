4d789d3da15d1c45eb113957bcde7c1c9ca4ff4f
smallmenu <smallmenu@gmail.com>
2022-08-11 18:08:17 +0800

    * update

--- lang_test.go
+++ lang_test.go
@@ -57,29 +57,29 @@ func BenchmarkLinguaTest(b *testing.B) {
 func TestLang(t *testing.T) {
 
 	var urlStrs = []string{
-		// "https://www.163.com",
-		//
-		// "https://english.news.cn",
-		// "https://jp.news.cn",
-		// "https://kr.news.cn",
-		// "https://german.news.cn/",
-		// "https://portuguese.news.cn/",
-		// "https://arabic.news.cn",
-		// "https://french.news.cn",
+		"https://www.163.com",
 
-		// "https://mn.cctv.com/",
-		//
-		// "https://www.bbc.com",
-		// "https://www.ft.com/",
-		//
-		// "http://government.ru",
-		//
-		// "https://www.gouvernement.fr",
-		//
-		// "http://live.siammedia.org/",
-		// "https://www.manchestereveningnews.co.uk/",
+		"https://english.news.cn",
+		"https://jp.news.cn",
+		"https://kr.news.cn",
+		"https://german.news.cn/",
+		"https://portuguese.news.cn/",
+		"https://arabic.news.cn",
+		"https://french.news.cn",
 
-		// "https://www.chinadaily.com.cn",
+		"https://mn.cctv.com/",
+
+		"https://www.bbc.com",
+		"https://www.ft.com/",
+
+		"http://government.ru",
+
+		"https://www.gouvernement.fr",
+
+		"http://live.siammedia.org/",
+		"https://www.manchestereveningnews.co.uk/",
+
+		"https://www.chinadaily.com.cn",
 		"http://cn.chinadaily.com.cn/",
 		"http://www.chinadaily.com.cn/chinawatch_fr/index.html",
 	}
