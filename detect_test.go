e2a921cc2a84c84d1cc409c60b7cebfcca7d01aa
smallmenu <smallmenu@gmail.com>
2022-08-15 18:08:45 +0800

    * update

--- detect_test.go
+++ detect_test.go
@@ -14,7 +14,7 @@ func TestDomainDetect(t *testing.T) {
 	domains := []string{
 		// "china-nengyuan.com",
 		// "suosi.com.cn",
-		"suosi.com.cn",
+		"gpyh.com",
 	}
 
 	for _, domain := range domains {
