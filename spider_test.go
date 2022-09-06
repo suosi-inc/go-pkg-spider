0430c55cb3db00916ce988d2c0754e204758cbbd
smallmenu <smallmenu@gmail.com>
2022-09-06 12:29:17 +0800

    * update

--- spider_test.go
+++ spider_test.go
@@ -108,7 +108,7 @@ func TestContent(t *testing.T) {
 		// "https://www.huxiu.com/article/651531.html",
 		// "http://www.news.cn/politics/2022-09/02/c_1128969463.htm",
 		// "https://www.ccdi.gov.cn/yaowenn/202209/t20220901_215343.html",
-		"http://news.4399.com/fzscjh/zixun/m/939117.html",
+		"http://news.4399.com/jlp/zhua/827413.html",
 	}
 
 	for _, urlStr := range urlStrs {
